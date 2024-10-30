82a800285be4214ea41e63c9050f65f28f03c3a8
Sofía González <sofia_1.6@hotmail.com>
2024-10-30 18:10:37 -0300

    cors2

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -8,9 +8,9 @@ import { JwtPayload } from '../auth/interfaces';
 
 
 
-// @WebSocketGateway({ cors: { origin: ['https://ws-nest-sg.netlify.app'] } })
+@WebSocketGateway({ cors: { origin: ['https://nest-ws-sg.netlify.app/'] } })
 
-@WebSocketGateway({ cors: true })
+// @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   @WebSocketServer() wss: Server;
