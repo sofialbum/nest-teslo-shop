75e9e1b8b85ac5e36eaf870ebdf4968b521d1c7e
Sofía González <sofia_1.6@hotmail.com>
2024-10-30 18:26:44 -0300

    cors4

--- src/main.ts
+++ src/main.ts
@@ -11,11 +11,10 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
 
-  // app.enableCors({
-  //   origin: 'http://localhost:5173', // Añade aquí todos los orígenes necesarios
-  //   methods: ['GET', 'POST'],
-  //   credentials: true,
-  // });
+  app.enableCors({
+    origin: 'https://nest-ws-sg.netlify.app/', // Añade aquí todos los orígenes necesarios
+    credentials: true,
+  });
 
   app.useGlobalPipes(  
     new ValidationPipe({ 
